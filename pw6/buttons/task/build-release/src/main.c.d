9db27e8283a873a3908386d88b067d53f9674256
Andrea Grillo <andrea.grillo@epfl.ch>
2023-12-01 10:53:34 +0100

    pw6

---
+++ pw6/buttons/task/build-release/src/main.c.d
@@ -0,0 +1,4 @@
+build-release/src/main.c.o: src/main.c support/include/stdio.h \
+ support/include/printf.h support/include/stdint.h \
+ support/include/cache.h support/include/defs.h support/include/spr.h \
+ support/include/vga.h support/include/switches.h support/include/swap.h
---
+++ pw6/buttons/task/build-release/support/src/uart.c.d
@@ -0,0 +1,3 @@
+build-release/support/src/uart.c.o: support/src/uart.c \
+ support/include/uart.h support/include/cache.h support/include/defs.h \
+ support/include/stdint.h support/include/spr.h
